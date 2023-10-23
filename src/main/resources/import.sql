ff228ffcadffc27c63cbcb1770a240c20c0e7d40
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2023-10-23 18:06:12 -0300

    feat: add styles with bootstrap

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,11 @@
 INSERT INTO cliente (cpf, email, endereco_entrega, nome,telefone) VALUES (82783123037, 'paulohenrique@gmail.com',  'Jardim Seila, Rua tal n78' , 'Paulo Henrique', 11977546821);
 
-INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Pizza Margherita', 'Classica italiana com molho de tomate, mussarela fresca, manjericão e um fio de azeite de oliva', 30, 'pizza');
+INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Pizza Margherita', 'Classica italiana com molho de tomate, mussarela fresca, manjericao e um fio de azeite de oliva', 30, 'pizza');
 INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Pizza Pepperoni', 'Picante com molho de tomate, queijo mussarela e generosas fatias de pepperoni', 35, 'pizza');
-INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Pizza Vegetariana', 'Repleta de vegetais frescos, como pimentão, cogumelos, azeitonas, cebola e queijo mussarela, sobre molho de tomate', 32, 'pizza');
-INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Pizza Quatro Queijos', 'Decadente com uma mistura de quatro queijos deliciosos - mussarela, gorgonzola, parmesão e provolone', 38, 'pizza');
-INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Pizza Frango com Catupiry', 'Cremosa com molho de catupiry, pedaços de frango grelhado, milho e cebola', 40, 'pizza');
+INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Pizza Vegetariana', 'Repleta de vegetais frescos, como pimentao, cogumelos, azeitonas, cebola e queijo mussarela, sobre molho de tomate', 32, 'pizza');
+INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Pizza Quatro Queijos', 'Decadente com uma mistura de quatro queijos deliciosos - mussarela, gorgonzola, parmesao e provolone', 38, 'pizza');
+INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Pizza Frango com Catupiry', 'Cremosa com molho de catupiry, pedacos de frango grelhado, milho e cebola', 40, 'pizza');
 
-INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Refrigerante Coca-Cola', 'Uma bebida carbonatada clássica, refrescante e doce que combina perfeitamente com pizza', 10, 'bebida');
-INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Suco de Laranja Natural', 'Um suco de laranja fresco, natural e cheio de vitaminas, perfeito para quem prefere uma opção não carbonatada', 7, 'bebida');
-INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Água Mineral', 'Uma escolha refrescante e saudável, perfeita para quem deseja se manter hidratado enquanto desfruta de pizza', 4, 'bebida');
+INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Refrigerante Coca-Cola', 'Uma bebida carbonatada classica, refrescante e doce que combina perfeitamente com pizza', 10, 'bebida');
+INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Suco de Laranja Natural', 'Um suco de laranja fresco, natural e cheio de vitaminas, perfeito para quem prefere uma opcao nao carbonatada', 7, 'bebida');
+INSERT INTO produto (nome, descricao, valor, tipo) VALUES ('Agua Mineral', 'Uma escolha refrescante e saudavel, perfeita para quem deseja se manter hidratado enquanto desfruta de pizza', 4, 'bebida');
